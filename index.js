45f09daaeefbd8608d5acc1f0c618ce02ba23925
centerback_rules@yahoo.com <1qazse4rfvgy7ujmko0p;/>
2017-11-16 09:42:13 -0700

    implement example stripe code

--- index.js
+++ index.js
@@ -4,25 +4,42 @@ var express = require('express'),
   route = require('./routes/routes.js'),
   bodyParser = require('body-parser');
 
-
 var app = express();
+app.post(path, (req, res) => {
+  const stripe_version = req.query.api_version;
+  if (!stripe_version) {
+    res.status(400).end();
+    return;
+  }
+  // This function assumes that some previous middleware has determined the
+  // correct customerId for the session and saved it on the request object.
+  stripe.ephemeralKeys.create(
+    {customer: req.customerId},
+    {stripe_version: stripe_version}
+  ).then((key) => {
+    res.status(200).json(key);
+  }).catch((err) => {
+    res.status(500).end();
+  });
+});
 
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
 
-app.use(express.static(path.join(__dirname + '/public')));
-
-var urlencodedParser = bodyParser.urlencoded({
-  extended: true
-})
-
-app.get('/', route.index);
-app.get('/create', route.create);
-app.get('/edit/:id', route.edit);
-app.get('/details/:id', route.details);
-app.post('/create', urlencodedParser, route.createPerson);
-app.post('/edit/:id', urlencodedParser, route.editPerson);
-app.get('/delete/:id', route.delete);
+//app.set('view engine', 'pug');
+//app.set('views', __dirname + '/views');
+//
+//app.use(express.static(path.join(__dirname + '/public')));
+//
+//var urlencodedParser = bodyParser.urlencoded({
+//  extended: true
+//})
+//
+//app.get('/', route.index);
+//app.get('/create', route.create);
+//app.get('/edit/:id', route.edit);
+//app.get('/details/:id', route.details);
+//app.post('/create', urlencodedParser, route.createPerson);
+//app.post('/edit/:id', urlencodedParser, route.editPerson);
+//app.get('/delete/:id', route.delete);
 
 app.listen(3000);
